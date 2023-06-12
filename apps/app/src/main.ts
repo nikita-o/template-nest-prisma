afe06a9c051928257c02fa7439167fbd0e7f2518
Nik <nik.oschepkov@yandex.ru>
2023-06-12 23:40:14 +0300

    +кринж конфиг

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import config from '../../../config/config.json';
+import { Logger } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(config.port, () => {
+    Logger.log(`server start: ${config.host}:${config.port}`, 'Main');
+  });
 }
 bootstrap();
